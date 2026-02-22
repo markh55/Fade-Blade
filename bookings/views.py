7621ef616700f6bcd7c0b9736fdee76dc18ae72d
markh55 <contact.codebymark@gmail.com>
2026-02-22 21:48:11 +0000

    updated views.py & created serializers.py

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Barber, TimeSlot, Booking
+from .serializers import BarberSerializer, TimeSlotSerializer, BookingSerializer
+from rest_framework import viewsets
 
 # Create your views here.
+class BarberViewSet(viewsets.ModelViewSet):
+    queryset = Barber.objects.all()
+    serializer_class = BarberSerializer
+
+class TimeSlotViewSet(viewsets.ModelViewSet):
+    queryset = TimeSlot.objects.all()
+    serializer_class = TimeSlotSerializer
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
